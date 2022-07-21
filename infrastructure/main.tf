406ecb3591d32d2af8fdb7c233d732cf730cf896
briandenicola <brian@briandenicola.com>
2022-07-21 03:37:11 -0500

    Default infrastructure

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,29 +9,30 @@ resource "random_id" "this" {
 }
 
 resource "random_pet" "this" {
-  length = 1
-  separator  = ""
+  length    = 1
+  separator = ""
 }
 
 resource "random_password" "password" {
-  length = 25
+  length  = 25
   special = true
 }
 
 locals {
-    location                    = "southcentralus"
-    resource_name               = "${random_pet.this.id}-${random_id.this.dec}"
+  location        = "southcentralus"
+  resource_name   = "${random_pet.this.id}-${random_id.this.dec}"
+  aks_name        = "${local.resource_name}-aks"
+  flux_path       = "./clusters/aks-01"
+  flux_repository = "https://github.com/briandenicola/aks-flux-extension"
 }
 
-
 resource "azurerm_resource_group" "this" {
-  name                  = "${local.resource_name}_rg"
-  location              = local.location
-  
-  tags     = {
-    Application = ""
-    Components  = ""
+  name     = "${local.resource_name}_rg"
+  location = local.location
+
+  tags = {
+    Application = "httpdemo"
+    Components  = "aks; flux"
     DeployedOn  = timestamp()
-    Deployer    = data.azurerm_client_config.current.principal_id
   }
 }
