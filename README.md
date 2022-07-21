d07839b83f528a857f2cc757b5ab7a0d4ce4caf1
Brian <brian@briandenicola.com>
2022-07-21 03:09:22 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Overview
+## Components 
+## Architecture
+# Infrastructure
+## Prerequisites
+## Deployment 
+# Application Deployment 
+# Validate 
+# Backlog 
---
+++ infrastructure/providers.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    azurerm  = {
+      source = "hashicorp/azurerm"
+      version = "3.3.0"
+    }
+    azuread = {
+      source = "hashicorp/azuread"
+      version = "2.18.0"
+    }
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.5.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features  {}
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = azurerm_kubernetes_cluster.this.kube_config.0.host
+
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.cluster_ca_certificate)
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.client_key)
+    username               = azurerm_kubernetes_cluster.this.kube_config.0.username
+    password               = azurerm_kubernetes_cluster.this.kube_config.0.password
+  }
+}
