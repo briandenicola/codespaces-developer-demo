7813291bae048ba182f921cb08d3b8a48627ba58
Brian <brian@briandenicola.com>
2022-07-30 04:55:13 +0000

    envsubst install

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,16 +4,13 @@
 
 echo "$(date)    post-create start" >> ~/status
 
-# Install golang v18.4
-#export VERSION=1.18.4
-#curl -fsS https://dl.google.com/go/go${VERSION}.linux-amd64.tar.gz -o golang.tar.gz
-#sudo tar -xvf golang.tar.gz
-#sudo rm -rf /usr/local/go
-#rm -rf /tmp/golang.tar.gz
-#sudo mv go /usr/local/bin
-
 # Install buildpacks
 (curl -sSL "https://github.com/buildpacks/pack/releases/download/v0.27.0/pack-v0.27.0-linux.tgz" | sudo tar -C /usr/local/bin/ --no-same-owner -xzv pack)
+
+# Install envsubst 
+curl -Lso envsubst https://github.com/a8m/envsubst/releases/download/v1.2.0/envsubst-Linux-x86_64
+sudo install envsubst /usr/local/bin
+rm -rf ./envsubst
 
 # Install draft
 curl -Lso draft https://github.com/Azure/draft/releases/download/v0.0.22/draft-linux-amd64
