6884d46a309b68ba4551bf66e902937472234568
Brian <brian@briandenicola.com>
2022-08-02 14:08:31 +0000

    Updated script

--- tests/curl-test.sh
+++ tests/curl-test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-pushd .
-
 cd ../infrastructure
 
 export APPLICATION_URI=`terraform output APPLICATION_URI | tr -d \"` 
@@ -11,5 +9,3 @@ echo Testing ${APPLICATION_URI} at ${APPLICATION_URI_IP}:443
 curl -k https://${APPLICATION_URI_IP}/api/os -H "Host: ${APPLICATION_URI}"
 
 # TBD - Add playwright test suite
-
-popd
