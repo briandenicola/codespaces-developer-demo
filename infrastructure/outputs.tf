2459e5699b9d575d50a4485c91c254e9988d0e7b
Brian <brian@briandenicola.com>
2022-09-07 15:00:22 +0000

    Output bugfix

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -14,7 +14,8 @@ output "ACR_NAME" {
 }
 
 output "primary_access_key" {
-  value = azurerm_redis_cache.this.primary_access_key
+    value = azurerm_redis_cache.this.primary_access_key
+    sensitive = true
 }
 
 output "CERTIFICATE_KV_URI" {
