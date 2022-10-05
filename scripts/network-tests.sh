01663eac14803a39eaea43b0a8d1e960e8e79d5c
Brian <brian@briandenicola.com>
2022-10-05 19:45:28 +0000

    Added verbose logging

--- scripts/network-tests.sh
+++ scripts/network-tests.sh
@@ -6,4 +6,4 @@ source ./scripts/setup-env.sh
 export APPLICATION_URI_IP=`kubectl -n app-routing-system get service nginx -o jsonpath={.status.loadBalancer.ingress[].ip}` 
 
 echo Testing ${APPLICATION_URI} at ${APPLICATION_URI_IP}:443
-curl -k https://${APPLICATION_URI_IP}/api/os -H "Host: ${APPLICATION_URI}"
+curl -vvk https://${APPLICATION_URI_IP}/api/os -H "Host: ${APPLICATION_URI}"
