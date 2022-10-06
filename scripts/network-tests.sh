16febd1b92df67de01395cdbace6f45426c8e96f
Brian <brian@briandenicola.com>
2022-10-06 03:39:44 +0000

    Fixed Network script with TLS

--- scripts/network-tests.sh
+++ scripts/network-tests.sh
@@ -6,4 +6,5 @@ source ./scripts/setup-env.sh
 export APPLICATION_URI_IP=`kubectl -n app-routing-system get service nginx -o jsonpath={.status.loadBalancer.ingress[].ip}` 
 
 echo Testing ${APPLICATION_URI} at ${APPLICATION_URI_IP}:443
-curl -vvk https://${APPLICATION_URI_IP}/api/os -H "Host: ${APPLICATION_URI}"
+#curl -vvk https://${APPLICATION_URI_IP}/api/os -H "Host: ${APPLICATION_URI}"
+curl -vvk https://${APPLICATION_URI}/api/os --resolve ${APPLICATION_URI}:443:${APPLICATION_URI_IP}
