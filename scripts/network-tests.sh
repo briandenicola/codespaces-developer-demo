da6346e073b9205f672bc66a4a3e455e7649a3c4
briandenicola <brian@briandenicola.com>
2023-01-09 10:34:25 -0600

    Better path handling

--- scripts/network-tests.sh
+++ scripts/network-tests.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-cd ..
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+source ${SCRIPT_DIR}/setup-env.sh
 
-source ./scripts/setup-env.sh
 export APPLICATION_URI_IP=`kubectl -n app-routing-system get service nginx -o jsonpath={.status.loadBalancer.ingress[].ip}` 
 
 echo Testing ${APPLICATION_URI} at ${APPLICATION_URI_IP}:443
-#curl -vvk https://${APPLICATION_URI_IP}/api/os -H "Host: ${APPLICATION_URI}"
 curl -vvk https://${APPLICATION_URI}/api/os --resolve ${APPLICATION_URI}:443:${APPLICATION_URI_IP}
--- scripts/playwright-tests.sh
+++ scripts/playwright-tests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cd ..
-source ./scripts/setup-env.sh
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+source ${SCRIPT_DIR}/setup-env.sh
 export APPLICATION_URI_IP=`kubectl -n app-routing-system get service nginx -o jsonpath={.status.loadBalancer.ingress[].ip}` 
 
 echo Testing ${APPLICATION_URI} at ${APPLICATION_URI_IP}:443
 
-cd tests
+cd ../tests
 npm install -D playwright@latest
 npx playwright test
