38274b2aa69251dc4508b7c58b2b6daa29c4db4b
Brian <brian@briandenicola.com>
2023-08-24 20:30:01 +0000

    Moved to azurerm_federated_identity_credential

--- infrastructure/identities.tf
+++ infrastructure/identities.tf
@@ -16,18 +16,11 @@ resource "azurerm_user_assigned_identity" "whatos_service_account_identity" {
   location            = azurerm_resource_group.this.location
 }
 
-resource "azapi_resource" "federated_identity_credential" {
-  schema_validation_enabled = false
-  name                      = local.workload_id
-  parent_id                 = azurerm_user_assigned_identity.whatos_service_account_identity.id
-  type                      = "Microsoft.ManagedIdentity/userAssignedIdentities/federatedIdentityCredentials@2022-01-31-preview"
-
-  location                  = azurerm_resource_group.this.location
-  body = jsonencode({
-    properties = {
-      issuer    = azurerm_kubernetes_cluster.this.oidc_issuer_url
-      subject   = "system:serviceaccount:whatos:${local.workload_id}"
-      audiences = ["api://AzureADTokenExchange"]
-    }
-  })
+resource "azurerm_federated_identity_credential" "federated_identity_credential" {
+  name                = local.workload_id
+  resource_group_name = azurerm_resource_group.this.name
+  audience            = ["api://AzureADTokenExchange"]
+  issuer              = azurerm_kubernetes_cluster.this.oidc_issuer_url
+  parent_id           = azurerm_user_assigned_identity.whatos_service_account_identity.id
+  subject             = "system:serviceaccount:whatos:${local.workload_id}"
 }
