c0fa86a43f50bb3ec1d679478580475c26d2ffb4
briandenicola <brian@briandenicola.com>
2023-12-22 11:44:03 -0600

    Removed duplicate resource

--- infrastructure/flux.tf
+++ infrastructure/flux.tf
@@ -1,15 +1,5 @@
 resource "azurerm_kubernetes_cluster_extension" "flux" {
   depends_on = [
-    //azapi_update_resource.post-configs,
-    azurerm_kubernetes_cluster.this
-  ]
-  name           = "flux"
-  cluster_id     = azurerm_kubernetes_cluster.this.id
-  extension_type = "microsoft.flux"
-}
-
-resource "azurerm_kubernetes_cluster_extension" "flux" {
-  depends_on = [
     azurerm_kubernetes_cluster.this
   ]
   name           = "flux"
